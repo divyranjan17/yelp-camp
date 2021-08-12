7730e6675d92f146d881d15108aa9e7b2a69a63e
Divy Ranjan <divyranjan90@gmail.com>
2021-08-12 23:30:33 +0530

    Finish basic styles and layouts bootstrap

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const server = '127.0.0.1: 27017';
 const database = 'yelp-camp';
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 mongoose.connect(`mongodb://${server}/${database}`, {
     useNewUrlParser: true,
@@ -20,7 +22,7 @@ db.once("open", ()=>{
 
 const app = express();
 
-
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 
@@ -30,7 +32,7 @@ app.use(methodOverride('_method'));
 // HOME route
 app.get('/',(req,res) => {
     res.render('home');
-});
+})
 
 // INDEX route
 app.get('/campgrounds', async(req,res) => {
@@ -41,7 +43,7 @@ app.get('/campgrounds', async(req,res) => {
 // CREATE route
 app.get('/campgrounds/new',(req, res) => {
     res.render('campgrounds/new');
-});
+})
 
 app.post('/campgrounds', async (req, res, next) => {
     const campground = new Campground(req.body.campground);
@@ -51,7 +53,7 @@ app.post('/campgrounds', async (req, res, next) => {
 
 // SHOW route
 app.get('/campgrounds/:id', async (req,res)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground});
 })
 
@@ -67,11 +69,11 @@ app.put('/campgrounds/:id/', async (req,res) => {
    res.redirect(`/campgrounds/${campground._id}`);
 })
 
-app.delete('/campgrounds/:id', catchAsync(async(req,res) => {
+app.delete('/campgrounds/:id', async(req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-}))
+})
 
 app.listen(3000, ()=> {
     console.log('Serving on port 3000');
